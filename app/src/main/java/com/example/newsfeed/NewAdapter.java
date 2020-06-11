f330557f102647c28e7b092d0f1ec17bbab88588
Mohamed Selim <dev.m.selim@gmail.com>
2020-06-11 22:04:30 +0200

    implement News Adapter

---
+++ app/src/main/java/com/example/newsfeed/NewAdapter.java
@@ -0,0 +1,41 @@
+package com.example.newsfeed;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class NewAdapter extends ArrayAdapter<News> {
+    public NewAdapter(@NonNull Context context, int resource, @NonNull List<News> news) {
+        super(context, 0, news);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        News currentNews = getItem(position);
+        TextView headline = (TextView) listItemView.findViewById(R.id.tv_headline);
+        TextView author = (TextView) listItemView.findViewById(R.id.tv_author);
+        TextView section = (TextView) listItemView.findViewById(R.id.tv_section);
+        TextView date = (TextView) listItemView.findViewById(R.id.tv_date);
+
+        headline.setText(currentNews.getHeadline());
+        author.setText(currentNews.getAuthor());
+        section.setText(currentNews.getSection());
+        date.setText(currentNews.getDate());
+
+        return listItemView;
+    }
+}
