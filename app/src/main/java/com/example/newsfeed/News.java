5b7b873478496fe0d72fa8a9f8299ccfb2bfb084
Mohamed Selim <dev.m.selim@gmail.com>
2020-06-12 21:20:40 +0200

    add url member variable

--- app/src/main/java/com/example/newsfeed/News.java
+++ app/src/main/java/com/example/newsfeed/News.java
@@ -5,12 +5,14 @@ public class News {
     private String mAuthor = "";
     private String mDate = "";
     private String mSection = "";
+    private String mUrl;
 
-    public News(String headline, String author, String section, String date) {
+    public News(String headline, String author, String section, String date, String url) {
         mHeadline = headline;
         mAuthor = author;
         mSection = section;
         mDate = date;
+        mUrl = url;
     }
 
     public String getHeadline() {
@@ -27,5 +29,9 @@ public class News {
 
     public String getSection() {
         return mSection;
+    }
+
+    public String getUrl() {
+        return mUrl;
     }
 }
