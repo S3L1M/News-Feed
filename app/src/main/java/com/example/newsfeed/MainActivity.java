fbdcfdb1dc152eed8dc6980a9730332d4f78b714
Mohamed Selim <dev.m.selim@gmail.com>
2020-06-12 22:57:06 +0200

    use Loader instead of calling in main

--- app/src/main/java/com/example/newsfeed/MainActivity.java
+++ app/src/main/java/com/example/newsfeed/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.newsfeed;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
 import android.content.Intent;
 import android.graphics.ColorSpace;
@@ -23,34 +27,16 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<News>> {
+
+    private NewsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<News> news1 = new ArrayList<News>();
-        for(int i=0;i<20;i++) {
-            news1.add(new News("This is a long headline filled with relatively lengthy text",
-                    "Mark Nickolson", "Technology", "25/05/2020", ""));
-        }
-
-        URL url = null;
-        ArrayList<News> news = new ArrayList<News>();
-        try {
-            url = NetworkUtils.createUrl();
-            String jsonResponse = NetworkUtils.makeHttpRequest(url);
-            news = NetworkUtils.parseJson(jsonResponse);
-        } catch (IOException e) {
-            e.printStackTrace();
-            Toast.makeText(this, "JSONException", Toast.LENGTH_LONG).show();
-        } catch (JSONException e) {
-            e.printStackTrace();
-            Toast.makeText(this, "IOException", Toast.LENGTH_LONG).show();
-        }
-
-        final NewsAdapter adapter = new NewsAdapter(this, news);
+        adapter = new NewsAdapter(this);
         ListView listView = (ListView) findViewById(R.id.listview);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -62,5 +48,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        getSupportLoaderManager().initLoader(0, null, this);
+    }
+
+    @NonNull
+    @Override
+    public Loader<ArrayList<News>> onCreateLoader(int id, @Nullable Bundle args) {
+        return new NewsLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<ArrayList<News>> loader, ArrayList<News> data) {
+        if (data != null) {
+            adapter.setNotifyOnChange(false);
+            adapter.clear();
+            adapter.setNotifyOnChange(true);
+            adapter.addAll(data);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<ArrayList<News>> loader) {
+
     }
 }
